9b140983e661ad92dc3395ef9c0aa7834c229f5d
Progloper <85923230+SuryaSekharNath@users.noreply.github.com>
2023-04-08 11:43:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let imgBox = document.querySelector("#imgBox"),
+qrImage = document.querySelector("#qrImage"),
+qrText = document.querySelector("#qrText");
+
+function generateQR()
+{
+    if(qrText.value.length > 0)
+    {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show");
+    }
+}
